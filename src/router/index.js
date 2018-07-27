57069d52fae8b45dea60fdf0995db583c6a44357
summer <596837849@qq.com>
2018-07-27 23:13:06 +0800

    c

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import order from '@/components/order/order'
+import orderconfirmation from '@/components/orderconfirmation/orderconfirmation'
 Vue.use(Router)
+
+// const search = (resolve) => {
+//   import('@components/search/search').then((module) => {
+//     resolve(module)
+//   })
+// }
 
 export default new Router({
   routes: [
@@ -10,6 +17,18 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      title: '预定页',
+      path: '/order',
+      name: 'order',
+      component: order
+    },
+    {
+      title: '确定页',
+      path: '/orderconfirmation',
+      name: 'orderconfirmation',
+      component: orderconfirmation
     }
   ]
 })
