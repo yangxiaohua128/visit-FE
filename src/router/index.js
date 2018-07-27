83be121e93dfcd0a1d0ca449a91ddcaf526ed050
maxiao <1758215541@qq.com>
2018-07-27 23:42:05 +0800

    change2

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import search from '@/components/search/search'
+import signin from '@/components/signin/signin'
 Vue.use(Router)
+
+// const search = (resolve) => {
+//   import('@components/search/search').then((module) => {
+//     resolve(module)
+//   })
+// }
 
 export default new Router({
   routes: [
@@ -10,6 +17,18 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      title: '搜索页',
+      path: '/search',
+      name: 'search',
+      component: search
+    },
+    {
+      title: '登录前我的页面',
+      path: '/signin',
+      name: 'signin',
+      component: signin
     }
   ]
 })
