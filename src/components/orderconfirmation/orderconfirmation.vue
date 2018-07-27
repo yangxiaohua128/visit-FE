9af7e0997fc3469054dbe81094d88b0b67c845e1
summer <596837849@qq.com>
2018-07-27 22:54:21 +0800

    a

---
+++ src/components/orderconfirmation/orderconfirmation.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="confirmation">
+   <header>
+     <img src="img/箭头左.png"/>
+     <p>选择班期和人数</p>
+   </header>
+   <content>
+     <div class="order"></div>
+   </content>
+   <div class="eg"></div>
+   <p class="p1">我的第一个p</p>
+ </div>
+</template>
+
+<script>
+    export default {
+        name: "order confirmation"
+    }
+</script>
+
+<style type="text/css">
+  .eg{
+    width: 50px;
+    height: 50px;
+    background-color: red;
+  }
+  .p1{
+    font-size: 48px;
+    color: #000;
+  }
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -4,7 +4,7 @@
 
 <script>
     export default {
-        name: "order confirmation"
+        name: "search"
     }
 </script>
 
