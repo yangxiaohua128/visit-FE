de0b61f9f6368925d1db435aa415e12951d07e3a
maxiao <1758215541@qq.com>
2018-07-30 22:45:35 +0800

    change

--- src/components/signin/signin.vue
+++ src/components/signin/signin.vue
@@ -10,17 +10,58 @@
     <div class="div3">积分：</div>
     <div class="div4"></div>
     <div class="div5">
-      <img src="./img/collection.png"/><span>我的收藏</span>
-      <img src="./img/order.png"/><span>我的订单</span>
-      <img src="./img/travel.png"/><span>未出行订单</span>
-      <img src="./img/payment.png"/><span>未付款订单</span>
-      <img src="./img/evaluate.png"/><span>待评价订单</span>
-      <img src="./img/refund.png"/><span>退款订单</span>
-      <img src="./img/coupon.png"/><span>优惠券</span>
-      <img src="./img/album.png"/><span>我的相册</span>
+      <div>
+        <img src="./img/collection.png"/><br/>
+        <span>我的收藏</span>
+      </div>
+      <div>
+        <img src="./img/order.png"/><br/>
+        <span>我的订单</span>
+      </div>
+      <div>
+        <img src="./img/travel.png"/><br/>
+        <span>未出行订单</span>
+      </div>
+      <div>
+        <img src="./img/payment.png"/><br/>
+        <span>未付款订单</span>
+      </div>
+      <div>
+        <img src="./img/evaluate.png"/><br/>
+        <span>待评价订单</span>
+      </div>
+      <div>
+        <img src="./img/refund.png"/><br/>
+        <span>退款订单</span>
+      </div>
+      <div>
+        <img src="./img/coupon.png"/><br/>
+        <span>优惠券</span>
+      </div>
+      <div>
+        <img src="./img/album.png"/><br/>
+        <span>我的相册</span>
+      </div>
+    </div>
+    <div class="div6">
+      <div>
+        <img src="./img/homepage.jpg"/><br/>
+        <span>首页</span>
+      </div>
+      <div>
+        <img src="./img/destination.jpg"/><br/>
+        <span>目的地</span>
+      </div>
+      <div>
+        <img src="./img/history.jpg"/><br/>
+        <span>浏览历史</span>
+      </div>
+     <div>
+       <img src="./img/my.jpg"/><br/>
+       <span>我的</span>
+     </div>
 
     </div>
-
   </div>
 </template>
 
@@ -35,16 +76,29 @@
   }
 </script>
 
-<style type="text/css">
+<style lang="scss">
   body{padding:0;margin:0;}
-  .signin{width:750px;}
+  .signin{width:750px;position:relative;}
   .div1{height:420px;margin-left:40px;margin-right:40px;position: relative;}
   #img1{position:absolute;right:30px;bottom:0;height:200px;width:280px;}
-  #img2{margin-top:27px;width:42px;height:46px;}
-  .div1 p{margin-top:130px;color:#424242;font-size:60px;border:1px black solid;}
-  .div1 div{width:150px;height:50px;background-color:#ffde39;color:#4b414c;font-size:36px;
-    text-align: center;line-height:50px;button:70px;left:0;}
+  #img2{width:42px;height:46px;position:absolute;top:27px;right:0;}
+  .div1 p{width:500px;margin-top:130px;color:#424242;font-size:60px;display: inline-block;}
+  .div1 div{width:300px;height:100px;background-color:#f9de57;color:#4b414c;font-size:40px;
+    text-align: center;line-height:100px;margin-top:55px;margin-left:0;display:inline-block;}
   .div2 ,.div4{height:10px;background-color:#efecec}
-  .div3{height:70px;font-size:15px;padding-left:29px;}
-  .div5 img{width:21px;height:21px;}
+  .div3{height:70px;font-size:31px;margin-left:56px;line-height:70px;}
+  .div5 div{
+      display: inline-block;margin-left:30px;
+      margin-top:45px;height:97px;width:150px;
+      img{width:42px;height:42px;padding-left:54px;}
+      span{display: block; padding-top:25px;font-size:30px;color:#4b414c;text-align:center;}
+    }
+  .div6{
+    width:750px;height:98px;background-color:#efecec;position: fixed;bottom:0;left:0;
+   div{
+       display:inline-block;width:150px;margin-left:30px;
+     img{width:42px;height:48px;padding-top:10px;padding-left:54px;}
+     span{color:#4b414c;font-size:22px;padding-top:8px;display:block;text-align:center;}
+   }
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import search from '@/components/search/search'
 import signin from '@/components/signin/signin'
+import signin2 from '@/components/signin2/signin2'
+import message from '@/components/message/message'
+import phone from '@/components/phone/phone'
+import register from '@/components/register/register'
 Vue.use(Router)
 
 // const search = (resolve) => {
@@ -14,21 +17,35 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      title: '搜索页',
-      path: '/search',
-      name: 'search',
-      component: search
-    },
-    {
       title: '登录前我的页面',
       path: '/signin',
       name: 'signin',
       component: signin
+    },
+    {
+      title: '登录后我的页面',
+      path: '/signin2',
+      name: 'signin2',
+      component: signin2
+    },
+    {
+      title: '手机号登录页面',
+      path: '/phone',
+      name: 'phone',
+      component: phone
+    },
+    {
+      title: '短信验证码登录页面',
+      path: '/message',
+      name: 'message',
+      component: message
+    },
+    {
+      title: '注册页面',
+      path: '/register',
+      name: 'register',
+      component: register
     }
+
   ]
 })
