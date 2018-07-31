020018eeaa54c3a22886086e23a9c5da7b645163
maxiao <1758215541@qq.com>
2018-07-31 10:02:32 +0800

    change

--- build/utils.js
+++ build/utils.js
@@ -34,16 +34,7 @@ exports.cssLoaders = function (options) {
       sourceMap: options.sourceMap
     }
   }
-<<<<<<< HEAD
-  const px2remLoader = {
-    loader: 'px2rem-loader',
-    options: {
-      remUnit: 75
-    }
-  }
 
-=======
->>>>>>> dev
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
     // const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
