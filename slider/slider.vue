aef93f89e675222946c8688cb6ae5b52c2146cf0
yrtw <1634828591@qq.com>
2018-07-31 23:27:43 +0800

    dev

---
+++ slider/slider.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="swiper-box" rel="swiperBox">
+    <swiper :options="swiperOption" class="swiper-container" ref="mySwiper">
+      <!-- 轮播项 -->
+      <swiper-slide v-for="banner in banners">
+        <img :src="banner" class="swiper-image">
+      </swiper-slide>
+      <!-- 轮播的小圆点 -->
+      <div class="swiper-pagination" slot="pagination">
+      </div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  import { swiper, swiperSlide } from 'vue-awesome-swiper'
+  export default {
+    data:function() {
+      return {
+      }
+    },
+    props:{
+      banners:{
+        type:Array
+      },
+      swiperOption:{
+      }
+    },
+    components:{
+      swiper:swiper,
+      swiperSlide:swiperSlide
+    },
+    //定义这个sweiper对象
+    computed: {
+      swiper:function() {
+        return this.$refs.mySwiper.swiper;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  @import "swiper.css";
+  .swiper-box{
+    width: 100%;
+    height: 180px;
+    margin: 0 auto;
+  }
+  .swiper-box .swiper-container{
+    width: 100%;
+    height: 100%;
+  }
+  .swiper-box .swiper-box img{
+    width: 100%;
+    height: 100%;
+  }
+</style>
+</style>
